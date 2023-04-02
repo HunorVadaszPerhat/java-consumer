268fa403d7ae5d356ae50adaf2d778a8fcee9a38
Hunor Vadasz-Perhat <vadaszperhat.hunor@gmail.com>
2023-04-02 15:15:28 +0200

    added consumer accept

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# java-consumer
+# JAVA consumer
+
+## Consumer
+* accept:
+    * performs operation on the given argument
+    * void accept(T t)
+    *  [link](https://github.com/HunorVadaszPerhat/java-predicate/tree/main/predicate/predicate_test)
---
+++ consumer/consumer_accept/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Consumer<String> c = (x) -> System.out.println(x.toLowerCase());
+        c.accept("Java2s.com");
+        // Output: java2s.com
+    }
+}
